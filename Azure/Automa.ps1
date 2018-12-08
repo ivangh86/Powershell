b0565f3833de76f588d58a2992c7727d318ba414
unknown <ivan1986.19@gmail.com>
2018-12-08 19:48:08 +0100

    commit

--- Azure/Automa.ps1
+++ Azure/Automa.ps1
@@ -1,18 +1,46 @@
 $RGs = Get-AzureRmResourceGroup
 
-$VMList= @ ()
-
+$VMList= @()
 foreach ($RG in $RGs)
 {
-
     $VMs = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName
-    foreach ($VM in VMs)
-
+    foreach ($VM in $VMs)
     {
         $VMDetail = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name -Status
+        foreach ($VMStatus in $VMDetail.Statuses)
+        {
+            if ($VMStatus.Code.CompareTo("ProvisioningState/succeeded") -ne 0 ) # don't want to the provsioning status
+            {
+                $VMStatusDetail = $VMStatus.DisplayStatus
 
-
+            }
+        }
+    }   
+    $VMList += [PSCustomObject]@{
+        "Name"=$VM.Name; "Status"=$VMStatusDetail; "ResourceGroup" = $VM.ResourceGroupName
     }
 
-
 }
+$VMList
+
+
+#############################################
+
+#View all Azure locations
+Get-AzureRmLocation
+
+$azurelocations = Get-AzureRmLocation
+$resources = Get-AzureRmResourceProvider -ProviderNamespace 'Microsoft.Compute'
+$resources.ResourceTypes.Where{($_.ResourceTypeName -eq 'virtualMachines')}.locations
+
+Get-AzureRmVMSize -Location "North Europe"
+#############################################
+
+# Actions on all VMs in RG
+$rgname = 'RGNEInfra'
+$VMs = Get-AzureRMVm -ResourceGroupName $rgname
+
+foreach ($VM in $VMs)   
+    {
+        Stop-AzureRmVM -Name $VM.Name -ResourceGroupName $rgname -Force
+    }   
