816f548fa41b26fb435ffdacb837155275f02868
unknown <ivan1986.19@gmail.com>
2018-12-08 18:32:23 +0100

    comit

--- Azure/createazurermvm.ps1
+++ Azure/createazurermvm.ps1
@@ -11,17 +11,18 @@ $loc = 'northeurope'
 
 #New-AzureRmResourceGroup -Name $rgname -Location $loc -Verbose -Force -ErrorAction Stop
 
-#New-AzureRmVirtualNetwork -ResourceGroupName $rgname -Name $virtnetname -Location $loc -AddressPrefix 10.1.0.0/24 
+
 
 #New-AzureRmStorageAccount -ResourceGroupName $rgname -Name $stoname -Location $loc -Type $stotype
 #$stoaccount = Get-AzureRmStorageAccount -ResourceGroupName $rgname -Name $stoname 
 
 #Create VM Object
-#$vm = New-AzureRmVMConfig -VMName $vmname -VMSize $vmsize
+$vm = New-AzureRmVMConfig -VMName $vmname -VMSize $vmsize
 
 # Setup Networking
-# $subnet = New-AzureRmVirtualNetworkSubnetConfig -Name ('subnet' + $rgname)  -AddressPrefix "10.0.0.0/24"
-# New-AzureRmVirtualNetwork -Force -Name ('vnet'+ $rgname) -ResourceGroupName $rgname -Location $loc -AddressPrefix 10.0.0.0/16 -DnsServer 10.1.1.1 -Subnet $subnet
+#$subnet = New-AzureRmVirtualNetworkSubnetConfig -Name ('subnet' + $rgname)  -AddressPrefix "10.0.0.0/24"
+New-AzureRmVirtualNetwork -ResourceGroupName $rgname -Name $virtnetname -Location $loc -AddressPrefix 10.0.0.0/16 -DnsServer "10.1.1.1" -Subnet $subnet
+$vnet = New-AzureRmVirtualNetwork -Force -Name ('vnet'+ $rgname) -ResourceGroupName $rgname -Location $loc -AddressPrefix 10.0.0.0/16 -DnsServer 10.1.1.1 -Subnet $subnet
 $vnet = Get-AzureRmVirtualNetwork -Name $virtnetname -ResourceGroupName $rgname
 $subnetID = $vnet.Subnets[0].Id
 
@@ -29,7 +30,7 @@ $pip = New-AzureRmPublicIpAddress -ResourceGroupName $rgname -Name "vip1" `
 -Location $loc -AllocationMethod Dynamic -DomainNameLabel $vmname.ToLower()
 
 $nic = New-AzureRmNetworkInterface -Force -Name ('nic' + $vmname) `
--ResourceGroupName $rgname -Location $loc -SubnetId $subnetID -PrivateIPAddress 10.7.115.13 -DnsServer 10.7.115.13 `
+-ResourceGroupName $rgname -Location $loc -SubnetId $subnetID -PrivateIPAddress 10.0.0.13 -DnsServer 10.0.0.13 `
 -PublicIPAddressID $pip.Id
 
 
@@ -39,7 +40,7 @@ $vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
 # Using an Azure image
 $osDiskName = $vmname+'osDisk'
 $osDiskCaching = 'ReadWrite'
-$osDiskVhdUri = "https://$stoname.blob.core.winwdos.net/vhds/"+$vmname+"-OS.vhd"
+$osDiskVhdUri = "https://$stoname.blob.core.windows.net/vhds/"+$vmname+"-OS.vhd"
 # Setup OS & Image
 $user = "localadmin"
 $password = 'Pa55word5'
@@ -59,15 +60,26 @@ $AzureImage= $AzureImageSku[0]
 # Get-AzureRmVMImageOffer -Location $loc -PublisherName "MicrosoftWindowsServer" 
 # Get-AzureRmVMImageSku -Location $loc -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer"
 
-$vm = Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $vmname -Credential $cred
-$vm = Set-AzureRmVMSourceImage -VM $vm -PublisherName $AzureImage.PublisherName -Offer $AzureImage.Offer -Skus $AzureImage.Skus
+$vm = Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $vmname -Credential $cred 
+$vm = Set-AzureRmVMSourceImage -VM $vm -PublisherName $AzureImage.PublisherName -Offer $AzureImage.Offer -Skus $AzureImage.Skus -Version $AzureImage.Version
 $vm = Set-AzureRmVMOSDisk -VM $vm -VhdUri $osDiskVhdUri -Name $osDiskName -CreateOption fromImage -Caching $osDiskCaching
 
 
 # To create VM from a custom VHD replace with https://azure.microsoft.com/en-en/documentation/articles/virtual-machines-windows
-$sourcevhd = "https://$stoname.blob.core.windows.net/vhds/win2012r2custom-OS.vhd"
-$osDiskVhdUri = "https://$stoname.blob.core.windows.net/vhds/"+$vmname+"-OS.vhd"
-$vm = Set-AzureRmVMOSDisk -VM $vm -Name $osDiskName -VhdUri $osDiskVhdUri - -CreateOption 'fromImage' -SourceImageUri $sourcevhd -Windows -Caching $osDiskCaching
+#$sourcevhd = "https://$stoname.blob.core.windows.net/vhds/win2012r2custom-OS.vhd"
+#$osDiskVhdUri = "https://$stoname.blob.core.windows.net/vhds/"+$vmname+"-OS.vhd"
+#$vm = Set-AzureRmVMOSDisk -VM $vm -Name $osDiskName -VhdUri $osDiskVhdUri - -CreateOption 'fromImage' -SourceImageUri $sourcevhd -Windows -Caching $osDiskCaching
  
 # To create VM from an existing disk replace Using an Azure image commands with:
-$vm = Set-AzureRmVMOSDisk -VM $vm -VhdUri $osDiskVhdUri -Name $osDiskName -CreateOption 'attach' -Windows -Caching $osDiskCaching
+#$vm = Set-AzureRmVMOSDisk -VM $vm -VhdUri $osDiskVhdUri -Name $osDiskName -CreateOption 'attach' -Windows -Caching $osDiskCaching
+
+
+#Add a data disk
+#$dataDiskVhdUri = "https://$stoname.blob.core.windows.net/vhds/"+$vmname+"-Data.vhd"
+#$dataDiskName = $vmname+'_dataDisk'
+#$vm = Add-AzureRmVMDataDisk -VM $vm -Name $dataDiskName -Caching None -CreateOption 'attach' -DiskSizeInGB 1023 -VhdUri $dataDiskVhdUri
+
+
+# Create virtual machine
+
+New-AzureRmVM -ResourceGroupName $rgname -Location $loc -VM $vm
