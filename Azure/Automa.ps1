2728abe54f0f02e2add45526690f0105282d2243
unknown <ivan1986.19@gmail.com>
2018-12-14 20:16:59 +0100

    commit

--- Azure/Automa.ps1
+++ Azure/Automa.ps1
@@ -55,7 +55,7 @@ foreach ($storact in $storacts)
 
 
 # View Availability Set information for VMs
-
+# Debes tener un conjunto de disponibilidad creado (Se crea al crear la maquina)
 Get-AzureRmVM | Where-Object {$_.ResourceGroupName -eq 'RGNEInfra' } | Format-Table Name,ResourceGroupName -AutoSize
 $AS = Get-AzureRmAvailabilitySet -ResourceGroupName 'RGNEInfra' 
 $AS.VirtualMachinesReferences | ForEach-Object {$VMResource = (Get-AzureRmResource -Id $_.Id);
@@ -63,3 +63,38 @@ $AS.VirtualMachinesReferences | ForEach-Object {$VMResource = (Get-AzureRmResour
     [PSCustomObject]@{ "Name" = $VM.Name; "FaultDomain"=$VM.PlatformFaultDomain; "UpateDomain"=$VM.PlatformUpdateDomain;
     }
 }
+
+# Capture an image @azcaptimage
+
+Stop-AzureRmVM -ResourceGroupName "RGNEInfra" -Name 'savazudc01' -Force
+Set-AzureRmVM -ResourceGroupName "RGNEInfra" -Name 'savazudc01' -Generalized
+Save-AzureRmVMImage -ResourceGroupName "RGNEInfra" -Name "savazudc01" -DestinationContainerName "custtemp" -VHDNamePrefix "template" -Path "e:\temp\CapTemple.json" 
+
+# Inject Powershell into a VM
+$rgname = 'RGNEInfra'
+$vmname = 'savazudc01';
+$loc = 'northeurope'
+
+$ScriptBlobAccount = 'sanegrsinfra'
+$ScriptBlobKey = "U9tKrSQ1iK8UpVZsZb/1sBqeD3SeOxyH0QNTizNhkNZXudha+CQDU4RWUSZKf88jRAVHB/l28p3oPXipDOM6MQ=="
+$ScriptBlobURL =  "https://sanegrsinfra.blob.core.windows.net/scripts/"
+
+$ScriptName = "outcomputername.ps1"
+$ExtensionName = "outcomputername"
+$ExtensionType = 'CustomScriptExtension'
+$Publisher = 'Microsoft.Compute'
+$Version = '1.8'
+$timestamp = (Get-Date).Ticks
+
+$ScriptLocation = $ScriptBlobURL + $ScriptName
+$ScriptExe = ".\$ScriptName"
+
+$PrivateConfiguration = @{"storageAccountName" = "ScriptBlobAccount"; "storageAccountKey" = "$ScriptBlobKey"}
+$PublicConfiguration = @{"fileUris" = [Object[]]"$ScriptLocation";"timestamp = $timestamp"; "commandToExecute"  = "powershell.exe -ExecutionPolicy Unrestricted"
+
+Set-AzureRmVMExtension -ResourceGroupName "RGNEInfra" -VMName $vmname -Location $loc `
+-Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionType -TypeHandlerVersion $Version `
+-Settings $PublicConfiguration #-ProtectedSettings $PrivateConfiguration
+
+# Should see its name as the Message as thats what the PowerShell runs
+((Get-AzureRmVM -Name $vmname -ResourceGroupName $rgname -Status).Extensions |Where-Object {$_.Name -eq $ExtensionName}).Substatuses
